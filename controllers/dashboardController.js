4c0cb939615001f61e090cfdc44cc33fa1ab5597
August Tornea <august.tornea@gmail.com>
2023-06-06 16:06:27 -0400

    controllers dashboardController findAll and sessions

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,3 +1,20 @@
 const Blog = require('../models/Blog');
 const User = require('../models/User');
 
+
+
+exports.dashboard = (req, res) => {
+  if (req.session.user) {
+    const userId = req.session.user.id;
+    Blog.findAll({ where: { belongsto: userId } })
+      .then(posts => {
+        res.render('dashboard', { posts, loggedInUser: req.session.user });
+      })
+      .catch(err => {
+        console.error('Error retrieving posts:', err);
+        res.render('error');
+      });
+  } else {
+    res.redirect('/login');
+  }
+};
