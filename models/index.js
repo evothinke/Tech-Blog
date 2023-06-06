68f88951fa3554197874bef78129a85f1fc074b2
August Tornea <august.tornea@gmail.com>
2023-06-06 15:29:28 -0400

    adding models index

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const config = require('../config/config.json');
+const Comments = require('../models/Comments');
+
+
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env].database, config[env].username, config[env].password, config[env]);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.Blog = require('./Blog')(sequelize, Sequelize);
+db.User = require('./User')(sequelize, Sequelize);
+
+module.exports = db;
