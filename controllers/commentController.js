0e669d929163d546f0bed6cb58f6aa75a0326ddf
August Tornea <august.tornea@gmail.com>
2023-06-06 16:28:11 -0400

    controllers crud for blogs

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,1 +1,18 @@
 const Comment = require('../models/Comments');
+
+exports.create = (req, res) => {
+  const { blogId } = req.params;
+  const { content } = req.body;
+
+  Comment.create({
+    content,
+    BlogId: blogId
+  })
+    .then(() => {
+      res.redirect(`/blog`);
+    })
+    .catch(err => {
+      console.error('Error creating comment:', err);
+      res.render('error');
+    });
+};
